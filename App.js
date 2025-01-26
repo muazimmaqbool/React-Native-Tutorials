18ee4590444908c7739fe33fca8a428d54f9966a
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-01-26 21:55:12 +0530

    fix

--- App.js
+++ App.js
@@ -76,8 +76,8 @@ const App = () => {
           {/* <C_Image/> */}
           {/* <D_ScrollView/> */}
           {/* <E_Button/> */}
-          <F_Pressable/>
-          {/* <G_Modal/> */}
+          {/* <F_Pressable/> */}
+          <G_Modal/>
           {/* <H_Statusbar/> */}
           {/* <I_ActivityIndicator/> */}
           {/* <J_Alert/> */}
