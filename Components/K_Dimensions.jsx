3aca5caf1ffe41cc78aa13477f2514822c6d289a
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-02-17 22:20:32 +0530

    Dimensions

--- Components/K_Dimensions.jsx
+++ Components/K_Dimensions.jsx
@@ -1,72 +1,72 @@
-import { StyleSheet, Text, View,Dimensions } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
 /*
-->useWindowDimensions is the preferred API for React components. Unlike Dimensions, it updates as the window's
- dimensions update. This works nicely with the React paradigm.
+ ->useWindowDimensions is the preferred API for React components. Unlike Dimensions, it updates as the window's
+   dimensions update. This works nicely with the React paradigm.
 
  ->Although dimensions are available immediately, they may change (e.g due to device rotation, foldable devices etc)
   so any rendering logic or styles that depend on these constants should try to call this function on every render,
    rather than caching the value (for example, using inline styles rather than setting a value in a StyleSheet).
  */
 
-const windowDimensions=Dimensions.get("window");
-const screenDimensions=Dimensions.get("screen");
+const windowDimensions = Dimensions.get("window");
+const screenDimensions = Dimensions.get("screen");
 
 /*
 Properties: 
-NAME	TYPE	DESCRIPTION
-window	ScaledSize	Size of the visible Application window.
-screen	ScaledSize	Size of the device's screen.
+  NAME	    TYPE	         DESCRIPTION
+  window	ScaledSize	 Size of the visible Application window.
+  screen	ScaledSize	 Size of the device's screen.
 */
 
 const K_Dimensions = () => {
-    const [dimensions, setdimensions] = useState({
-        window:windowDimensions,
-        screen:screenDimensions
-    });
-    //change: Fires when a property within the Dimensions object changes. The argument to the event handler is a DimensionsValue type object.
-    useEffect(() => {
-        const subscription = Dimensions.addEventListener(
-          'change',
-          ({window, screen}) => {
-            setdimensions({window, screen});
-          },
-        );
-        return () => subscription?.remove();
-      });
+  const [dimensions, setdimensions] = useState({
+    window: windowDimensions,
+    screen: screenDimensions,
+  });
+  //change: Fires when a property within the Dimensions object changes. The argument to the event handler is a DimensionsValue type object.
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener(
+      "change",
+      ({ window, screen }) => {
+        setdimensions({ window, screen });
+      }
+    );
+    return () => subscription?.remove();
+  });
   return (
     <View style={styles.container}>
-    <Text style={styles.header}>Window Dimensions</Text>
-    {Object.entries(dimensions.window).map(([key, value]) => (
-      <Text style={styles.text}>
-        {key} - {value}
-      </Text>
-    ))}
-    <Text style={styles.header}>Screen Dimensions</Text>
-    {Object.entries(dimensions.screen).map(([key, value]) => (
-      <Text style={styles.text}>
-        {key} - {value}
-      </Text>
-    ))}
-  </View>
-  )
-}
+      <Text style={styles.header}>Window Dimensions</Text>
+      {Object.entries(dimensions.window).map(([key, value]) => (
+        <Text style={styles.text}>
+          {key} - {value}
+        </Text>
+      ))}
+      <Text style={styles.header}>Screen Dimensions</Text>
+      {Object.entries(dimensions.screen).map(([key, value]) => (
+        <Text style={styles.text}>
+          {key} - {value}
+        </Text>
+      ))}
+    </View>
+  );
+};
 
-export default K_Dimensions
+export default K_Dimensions;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      header: {
-        fontSize: 20,
-        marginVertical: 10,
-        color:"yellow"
-      },
-      text:{
-        color:"white",
-        fontSize:18
-      }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 20,
+    marginVertical: 10,
+    color: "yellow",
+  },
+  text: {
+    color: "white",
+    fontSize: 18,
+  },
+});
