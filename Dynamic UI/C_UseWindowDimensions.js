004b9221d11e54b83443bf96c5e7a2c5c7b780e5
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-03-06 22:34:17 +0530

    useWindowDimensions hook example

--- Dynamic UI/C_UseWindowDimensions.js
+++ Dynamic UI/C_UseWindowDimensions.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,useWindowDimensions } from "react-native";
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
 import React from "react";
 /*
   useWindowDimensions hook : is easy way to implement styles/changes when device dimensions change
@@ -7,33 +7,57 @@ import React from "react";
     ->its better to use useWindowDimensions over dimensions API
 */
 const C_UseWindowDimensions = () => {
-    const windowWidth=useWindowDimensions().width
-    const windowHeight=useWindowDimensions().height
-    console.log("width:",windowWidth,", height:",windowHeight)
-    return (
-        <View style={[styles.container,{backgroundColor:windowHeight>500?"skyblue":"plum"}]}>
-          <View style={[styles.box,{
-            width:windowWidth>500?"70%":"90%",
-            height:windowHeight>600?"60%":"90%"
-          }]}>
-            <Text style={{fontSize:windowWidth>500?50:24,color:windowWidth>500?"white":"black"}}>Welcome!</Text>
-          </View>
-        </View>
-      );
-}
+  const windowWidth = useWindowDimensions().width;
+  const windowHeight = useWindowDimensions().height;
+  //console.log("width:",windowWidth,", height:",windowHeight)
+  return (
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: windowHeight > 500 ? "skyblue" : "plum" },
+      ]}
+    >
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 600 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text
+          style={
+            windowWidth>500?styles.textBig:styles.textSmall
+          }
+        >
+          Welcome!
+        </Text>
+       {windowWidth > 500 &&  <Text style={styles.textSmall}>You Rotated The Device</Text>}
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    box: {
-      backgroundColor: "lightblue",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  });
-  
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textSmall:{
+    fontSize:24,
+    color:"blakc",
+  },
+  textBig:{
+    fontSize:50,
+    color:"white",
+  }
+});
 
-export default C_UseWindowDimensions
+export default C_UseWindowDimensions;
