ef36e52e38967170254a13916819998eb0f80151
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-03-10 10:54:20 +0530

    text input more props

--- Inputs and Forms/C_TextInput_props.js
+++ Inputs and Forms/C_TextInput_props.js
@@ -8,16 +8,16 @@ import {
 } from "react-native";
 import React, { useState } from "react";
 /*
-TextInput props allow you to customize the behavior and appearance of textinput component.
+  TextInput props allow you to customize the behavior and appearance of textinput component.
   like, placeholder, secureTextEntry, keyboardType,
 
   ?these two props are by default :
-   1) autoCorrect (which corrects the text) 2)autoCapitalize (which capitalizes the tex)
+  1)autoCorrect (which corrects the text) 2)autoCapitalize (which capitalizes the text)
     to make them off do this : autoCorrect={false} , autoCapitalize="none"
 
   secureTextEntry: masks the type text (used for entering password)
   keyboardType: allows you to specify the type of the keyboard that appears when the user interacts with the text input
-    example if user needs to enter phone number then you can use keyboardType="numeric"
+  example if user needs to enter phone number then you can use keyboardType="numeric"
 */
 
 const C_TextInput_props = () => {
@@ -32,6 +32,7 @@ const C_TextInput_props = () => {
         value={email}
         onChangeText={setEmail}
         placeholder="email@example.com"
+        keyboardType="email-address"
       />
       <TextInput style={styles.input} secureTextEntry placeholder="password" />
       <TextInput style={styles.input} placeholder="phone number" keyboardType="numeric"/>
