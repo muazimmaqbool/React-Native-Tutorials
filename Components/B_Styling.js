30504ce73168c8d64cf78ec241045f9e91db88ee
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-07-06 16:19:35 +0530

    inline styling

--- Components/B_Styling.js
+++ Components/B_Styling.js
@@ -4,15 +4,15 @@ import { Text, View } from 'react-native'
 /*
 ->Styling Your App
   -React Native didn't use css for styling instead it uses Javascript
-  -the names and values are same as css web but with slight modification like using camle case
-       i.e backgroundColor instead of background-color
+  -The names and values are same as css web but with slight modification like using camle case
+    i.e backgroundColor instead of background-color
 
   ->Styling Approaches:
     1)inline styling:
         i.e <Text style={{color:"white",padding:10,..}}>.....</Text>
     2)StyleSheet API: 
        it allows the user to define multiple styles at one place using "create" method
-      ->Its mostly used
+       it's mostly used
 */
 
 //Here only inline styling is used
