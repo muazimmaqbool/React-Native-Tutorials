ca18dcf4c3e7128143eac9e7f3ba8287357654be
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-07-08 23:04:59 +0530

    Vibrate

--- Components/J_Vibration.jsx
+++ Components/J_Vibration.jsx
@@ -1,25 +1,38 @@
-import { StyleSheet, Text, View ,Vibration, Button, Platform} from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Vibration,
+  Button,
+  Platform,
+} from "react-native";
+import React from "react";
 
 //Vibration API is used to Vibrate the device.
 const J_Vibration = () => {
-    const ONE_SECOND_IN_MS = 1000;
+  const ONE_SECOND_IN_MS = 1000;
 
-    const pattern=[
-        1 * ONE_SECOND_IN_MS,
-        2 * ONE_SECOND_IN_MS,
-        3 * ONE_SECOND_IN_MS,
-    ]
+  const pattern = [
+    1 * ONE_SECOND_IN_MS,
+    2 * ONE_SECOND_IN_MS,
+    3 * ONE_SECOND_IN_MS,
+  ];
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Vibration API</Text>
-      <Button title='Vibrate Once' onPress={()=>Vibration.vibrate()}/>
-      {Platform.OS === 'android'
-        ? <Button title='Vibrate For 5 Seconds' onPress={()=>Vibration.vibrate(5 * ONE_SECOND_IN_MS)}/>
-        : null}
-        {/* of Vibration.vibrate(5000)  to vibrate for 5 seconds*/}
-      
-      <Button title='Vibrate In Pattern' onPress={()=>Vibration.vibrate(pattern)}/>
+      <Button title="Vibrate Once" onPress={() => Vibration.vibrate()} />
+      {Platform.OS === "android" ? (
+        <Button
+          title="Vibrate For 5 Seconds"
+          onPress={() => Vibration.vibrate(5 * ONE_SECOND_IN_MS)}
+        />
+      ) : null}
+      {/* or Vibration.vibrate(5000)  to vibrate for 5 seconds*/}
+
+      <Button
+        title="Vibrate In Pattern"
+        onPress={() => Vibration.vibrate(pattern)}
+      />
 
       <Button
         title="Vibrate with pattern until cancelled"
@@ -32,18 +45,18 @@ const J_Vibration = () => {
         color="#FF0000"
       />
     </View>
-  )
-}
+  );
+};
 
-export default J_Vibration
+export default J_Vibration;
 
 const styles = StyleSheet.create({
-    container:{
-        alignItems:"center",
-        gap:20,
-    },
-    heading:{
-        fontSize:25,
-        color:"white",
-    }
-})
+  container: {
+    alignItems: "center",
+    gap: 20,
+  },
+  heading: {
+    fontSize: 25,
+    color: "white",
+  },
+});
