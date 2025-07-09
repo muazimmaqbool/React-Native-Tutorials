c9a266ef72da388cb45e460299747f1802f07ab0
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-07-09 23:09:07 +0530

    toast android

--- App.js
+++ App.js
@@ -95,8 +95,8 @@ const App = () => {
       {/* <I_UseWindowDimensions/> */}
       {/* <J_Vibration/> */}
       {/* <K_Dimensions/> */}
-      <L_BackHandler/>
-      {/* <M_ToastAndroid/> */}
+      {/* <L_BackHandler/> */}
+      <M_ToastAndroid/>
       {/* <N_Permissions/> */}
       {/* <O_AsyncStorage/> */}
 
